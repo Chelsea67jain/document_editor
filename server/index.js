7e18e195599ac3bf68f6811659ed89d2a1ced071
Chelsea Jain <chalsijain38@gmail.com>
2024-01-25 01:09:52 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 //const server=require('socket.io'); This code is used in ES5 but we can use the below code in ES6
 import { Server } from "socket.io";
 import Connection from "./database/db.js";
+import { getDocument,updateDocument } from "./controller/document_controller.js";
 
 Connection();
 const PORT=9000;
@@ -13,13 +14,18 @@ const io = new Server(PORT, {
 });
 
 io.on('connection',socket=>{
-  socket.on('get-document',documentId=>{
+  socket.on('get-document',async documentId=>{
     const data=""
+    var document=await getDocument(documentId);
     socket.join(documentId);
-    socket.emit(load-document,data);
+    socket.emit('load-document',document.data);
   socket.on('send-changes',delta=>{
     socket.broadcast.to(documentId).emit('receive-changes',delta);
   })
+  socket.on('save-changes',async data=>{
+    return updateDocument(documentId,data)
+  })
+
 })
 })
 
